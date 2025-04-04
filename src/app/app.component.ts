f84269da2f5f08201d342e73b1c596e4c9c76f87
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-04-04 23:14:21 +0100

    Home and Header Creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  template: `
+    <app-header  />
+    <app-home />
+  `
 })
 export class AppComponent {
   title = 'first-angular-app';
