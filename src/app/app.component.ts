cc898f2a853e423241d77f25b7f867cff79c23a7
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-04-08 20:25:30 +0100

    Navbar design, strings and dataTypes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { HeaderComponent } from './components/header/header.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   template: `
-    <app-header  />
+    <app-header />
     <app-home />
   `
 })
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,6 +6,19 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
 
+export class HeaderComponent {
+  logo: string = "FirstAngularApp"
+  link1: string = "Home"
+  link2: string = "About"
+  link3: string = "Contact"
+  // firstName: string = "Angular Tutorial";
+
+  // version: number = 18;
+
+  // isActive : boolean = false;
+
+  // currentDate: Date = new Date();
+
+  // inputType: string = "radio"; 
 }
