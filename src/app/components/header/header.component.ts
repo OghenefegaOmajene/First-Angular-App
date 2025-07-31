b56b95b22d2ea7cd32c3772af25f20095f1e4cf2
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-07-31 04:19:19 +0100

    Img structuring

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -12,6 +12,7 @@ export class HeaderComponent {
   link1: string = "Home"
   link2: string = "About"
   link3: string = "Contact"
+  logoPath: string = '../../../assets/images/logo.png'
   // firstName: string = "Angular Tutorial";
 
   // version: number = 18;
